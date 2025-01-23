163c4368f36e4df9cf216c2e2e737ed70fb33a89
NotMarra <marra313cz@gmail.com>
2025-01-23 22:47:59 +0100

    gradient show

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,16 @@
 	import { Button } from '$lib/components/ui/button';
 </script>
 
-<main class="h-full text-white bg-black"> <!-- radial gradient -->
+<main class="h-full text-white bg-black -z-20 relative">
+	<div class="absolute w-full overflow-hidden h-[100vh] flex justify-center -z-10">
+		<div
+			class="w-[80%] h-[125vh] blur-2xl bg-gradient-radial from-sky-600 via-transparent opacity-50 to-transparent absolute -top-1/2 mx-auto"
+		></div>
+		<div
+			class="w-[110vw] h-[150vh] blur-2xl bg-gradient-radial from-sky-700 via-transparent opacity-25 to-transparent absolute -top-1/2 mx-auto"
+		></div>
+	</div>
+	<!-- radial gradient -->
 	<header class="flex justify-between p-10 h-20 items-center">
 		<h1 class="text-3xl font-bold">NotTranslate</h1>
 		<Button class="bg-white text-black">Sign in</Button>
@@ -12,10 +21,11 @@
 		<div>
 			<h2 class="text-5xl">AI-Powered Subtitle Translation</h2>
 			<p class="text-lg mt-10 w-3/4">
-				NotTranslate uses cutting edge AI to translate your subtitles quickly and accurately, supporting ASS format and more.
+				NotTranslate uses cutting edge AI to translate your subtitles quickly and accurately,
+				supporting ASS format and more.
 			</p>
 		</div>
-		<img src="https://placehold.co/200x200" alt="">
+		<img src="https://placehold.co/200x200" alt="" />
 	</section>
 	<div class="place-self-center w-100% flex gap-5 mt-10 mb-10">
 		<Button class="bg-white text-black hover:bg-white">Start for free</Button>
@@ -24,20 +34,31 @@
 	<section class="flex flex-col w-full p-20 mt-20">
 		<h2 class="text-5xl self-center">AI-Driven features</h2>
 		<div class="flex justify-center gap-10 mt-20 w-full">
-			<div class="border-2 border-blue-500 p-10 rounded-2xl flex flex-col items-center min-h-[450px] w-[350px]">
-				<img class="" src="https://placehold.co/150x150" alt="" srcset="">
+			<div
+				class="border-2 border-notmarra_primary p-10 rounded-2xl flex flex-col items-center min-h-[450px] w-[350px] shadow-md shadow-notmarra_primary relative overflow-hidden"
+			>
+				<div
+					class="absolute inset-0 blur-2xl size-96 bg-gradient-radial from-notmarra_primary via-transparent to-transparent -top-1/3 opacity-40"
+				></div>
+				<div class="z-10 w-full flex flex-col items-center">
+					<img class="" src="https://placehold.co/150x150" alt="" srcset="" />
+					<h3 class="text-4xl mt-5">Header</h3>
+					<p class="mt-10">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+				</div>
+			</div>
+
+			<div
+				class="border-2 border-blue-500 p-10 rounded-2xl flex flex-col items-center min-h-[450px] w-[350px]"
+			>
+				<img class="" src="https://placehold.co/150x150" alt="" srcset="" />
 				<h3 class="text-4xl mt-5">Header</h3>
 				<p class="mt-10">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 			</div>
 
-			<div class="border-2 border-blue-500 p-10 rounded-2xl flex flex-col items-center min-h-[450px] w-[350px]">
-				<img class="" src="https://placehold.co/150x150" alt="" srcset="">
-				<h3 class="text-4xl mt-5">Header</h3>
-				<p class="mt-10">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-			</div>
-
-			<div class="border-2 border-blue-500 p-10 rounded-2xl flex flex-col items-center min-h-[450px] w-[350px]">
-				<img class="" src="https://placehold.co/150x150" alt="" srcset="">
+			<div
+				class="border-2 border-blue-500 p-10 rounded-2xl flex flex-col items-center min-h-[450px] w-[350px]"
+			>
+				<img class="" src="https://placehold.co/150x150" alt="" srcset="" />
 				<h3 class="text-4xl mt-5">Header</h3>
 				<p class="mt-10">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 			</div>
@@ -87,5 +108,4 @@
 			</div>
 		</div>
 	</section>
-
 </main>
